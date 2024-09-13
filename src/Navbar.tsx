8d37502726971eab7c9beaac681be02a9342ec0c
Perke1 <springerjan2@gmail.com>
2024-09-13 15:54:15 +0200

    feat: simple navbar

---
+++ src/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="https://x.com/soly2k">X</a>
+      <a href="https://t.me/soly2k">Telegram</a>
+      <a href="https://dexscreener.com/solana/D9EkfhvCpP7TqxpEXVXayoRR3vKbt5USfqs6ws61M189">
+        Chart
+      </a>
+    </div>
+  );
+}
