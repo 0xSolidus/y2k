e9c576481358abef48e388117e948633ce584e63
Perke1 <springerjan2@gmail.com>
2024-09-14 19:15:12 +0200

    refactor: removed unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { Routing } from './Routing.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <Routing></Routing>
-
 )
